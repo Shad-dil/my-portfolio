684e3321fdbc47f126534710e1821acf3df1c5e9
Dilshad <helloshad3@gmail.com>
2025-06-29 17:52:36 +0530

    working on skill section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,49 +1,65 @@
-const Contact = () => (
-  <section className="px-8 py-16 bg-white dark:bg-[#0f0f11]" id="contact">
-    <h2 className="text-3xl font-semibold mb-4">Get in Touch</h2>
+import { useState } from "react";
 
-    <p className="text-gray-600 dark:text-gray-400 mb-4">
-      Want to collaborate or just say hi? Let's connect.
-    </p>
-    <div className="space-y-2 text-sm grid grid-cols-2">
-      <div>
-        <a
-          href="mailto:hellodilshad3@gmail.com"
-          className="block hover:text-blue-500 dark:hover:text-blue-400"
-        >
-          hellodilshad3@gmail.com
-        </a>
-        <a
-          href="https://www.linkedin.com/in/shad-dil/"
-          target="_blank"
-          className="block hover:text-blue-500 dark:hover:text-blue-400"
-        >
-          LinkedIn
-        </a>
-        <a
-          href="https://github.com/Shad-dil"
-          target="_blank"
-          className="block hover:text-blue-500 dark:hover:text-blue-400"
-        >
-          GitHub
-        </a>
+const Contact = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState({});
+  const handleSubmit = () => {
+    if (!email || email.length === 0) error.email = "Please Enter email";
+    console.log("submittedd");
+  };
+  return (
+    <section className="px-8 py-16 bg-white dark:bg-[#0f0f11]" id="contact">
+      <h2 className="text-3xl font-semibold mb-4">Get in Touch</h2>
+
+      <p className="text-gray-600 dark:text-gray-400 mb-4">
+        Want to collaborate or just say hi? Let's connect.
+      </p>
+      <div className="space-y-2 text-sm grid grid-cols-2">
+        <div>
+          <a
+            href="mailto:hellodilshad3@gmail.com"
+            className="block hover:text-blue-500 dark:hover:text-blue-400"
+          >
+            hellodilshad3@gmail.com
+          </a>
+          <a
+            href="https://www.linkedin.com/in/shad-dil/"
+            target="_blank"
+            className="block hover:text-blue-500 dark:hover:text-blue-400"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/Shad-dil"
+            target="_blank"
+            className="block hover:text-blue-500 dark:hover:text-blue-400"
+          >
+            GitHub
+          </a>
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your Email id ..."
+            className="py-2 px-2 w-[500px] text-left text-gray-700 rounded-md "
+          />
+          <span>{error.email}</span>
+          <button
+            className="px-2 py-2 mx-3 bg-gray-700 rounded-md"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
       </div>
-      <div>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter your Email id ..."
-          className="py-2 px-2 w-[500px] text-left text-gray-700 rounded-md "
-        />
-        <button className="px-2 py-2 mx-3 bg-gray-700 rounded-md">
-          Submit
-        </button>
+      <div className="flex justify-center items-center my-4">
+        <p>Made with 💗</p>
       </div>
-    </div>
-    <div className="flex justify-center items-center my-4">
-      <p>Made with 💗</p>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 export default Contact;
