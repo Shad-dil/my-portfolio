6494a00d80c46fe1712e035e7311443f86ab9ab0
Dilshad <helloshad3@gmail.com>
2025-11-20 21:57:07 +0530

    changing hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        "background-color": "#1E1E2F",
+        background: "#1E1E2F",
         surface: "#151522",
-        "primary-color": "#4F46E5",
-        "secondary-color": "#6366F1",
+        textPrimary: "#4F46E5",
+        textSecondary: "#6366F1",
         accent: "#5E81AC",
-        "card-bg": "#1E293B",
+        card: "#1E293B",
         cta: "#BF616A",
         border: "#2C2F3F",
       },
